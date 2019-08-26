300a81925d642dfbec9aa11aaa497bfafd5cc3de
Paul Ashour <paul.ashour@gmail.com>
2019-08-26 09:38:52 -0600

    Add init controllers, configure routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get '/app', to: 'welcome#app', as: 'app'
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
