c4c0fe27d1869b9ec2d541b161dcfebede306f81
Paul Ashour <paul.ashour@gmail.com>
2019-09-08 12:34:33 -0600

    Finish configuring database and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_27_134334) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
