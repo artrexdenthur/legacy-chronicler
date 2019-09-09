1008539d0a25c384a786a43075c96c7cac55d8f0
Paul Ashour <paul.ashour@gmail.com>
2019-09-08 18:11:05 -0600

    Set Devise 401 behavior on login to app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
+  before_action :check_user, only: [:app]
   def home
   end
 
   def app
+  end
+
+  def check_user
+    return if user_signed_in?
+    flash[:notice] = "You must sign in or sign up before continuing"
+    redirect_to new_user_session_path, error: "You must sign in or sign up before continuing"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  # Token Login
+  # Basic Devise Login (Make sure this comes first)
+  devise_for :users
+
+  # Devise Token Auth Login
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 
-  # Basic Devise Login
-  devise_for :users
   
   get 'welcome/home'
   get '/app', to: 'welcome#app', as: 'app'
