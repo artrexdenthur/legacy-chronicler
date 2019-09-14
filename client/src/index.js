977ef21766f5a1177c15193f750cdf43ab35fe67
Paul Ashour <paul.ashour@gmail.com>
2019-09-14 13:26:18 -0600

    Fix react index

--- client/src/index.js
+++ client/src/index.js
@@ -43,7 +43,7 @@ ReactDOM.render(
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossOrigin="anonymous"
     />
-  </div>
+  </div>,
   // </Provider>,
   document.getElementById("root")
 );
