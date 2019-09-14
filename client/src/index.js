7f5ab32ef2dd4716d851898e499bc28060ba7ae4
Paul Ashour <paul.ashour@gmail.com>
2019-09-14 13:24:34 -0600

    Put precompile back in and try removing redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import Nav from "./components/Nav";
 
 // React Redux, Thunk, and related concerns
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
@@ -28,7 +28,7 @@ const store = createStore(
 );
 verifyCredentials(store); // Is the user already logged in?
 ReactDOM.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <Router>
       <Nav />
       <Route path="/" exact component={App} />
@@ -42,7 +42,7 @@ ReactDOM.render(
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossOrigin="anonymous"
     />
-  </Provider>,
+  // </Provider>,
   document.getElementById("root")
 );
 
