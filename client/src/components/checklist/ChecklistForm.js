59ae565778b6f48e36b84607311a9241281be11f
Paul Ashour <paul.ashour@gmail.com>
2019-10-16 06:43:23 -0600

    format

--- client/src/components/checklist/ChecklistForm.js
+++ client/src/components/checklist/ChecklistForm.js
@@ -5,44 +5,51 @@ import { Col } from 'react-bootstrap'
 
 class ChecklistForm extends Component {
   constructor() {
-    super()
-    this.handleOnSubmit = this.handleOnSubmit.bind(this)
-    this.handleOnInput = this.handleOnInput.bind(this)
+    super();
+    // this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.handleOnInput = this.handleOnInput.bind(this);
     this.state = {
       text: ""
-    }
+    };
   }
 
   handleOnSubmit(e) {
     e.preventDefault();
-    this.props.submitNewConcept({type: this.props.type, name: this.state.text})
+    this.props.submitNewConcept({
+      type: this.props.type,
+      name: this.state.text
+    });
     this.setState({
       text: ""
-    })
-    this.props.toggle()
-
+    });
+    this.props.toggle();
   }
 
   handleOnInput(e) {
     e.preventDefault();
     this.setState({
       text: e.target.value
-    })
+    });
   }
 
   render() {
     return (
       <form onSubmit={this.handleOnSubmit}>
         <label>
-          {"New " + this.props.type.slice(0, 1).toUpperCase() + this.props.type.slice(1)}
-          <input onChange={this.handleOnInput} type="text" name={this.props.type} value={this.state.text} />
-        <input type="submit" value="Submit"/>
+          {"New " +
+            this.props.type.slice(0, 1).toUpperCase() +
+            this.props.type.slice(1)}
+          <input
+            onChange={this.handleOnInput}
+            type="text"
+            name={this.props.type}
+            value={this.state.text}
+          />
+          <input type="submit" value="Submit" />
         </label>
       </form>
-    )
+    );
   }
-
-
 }
 
 const mapState = state => {
